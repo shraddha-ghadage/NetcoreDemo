d1775bbe8a048c9e5667991ac544c9b7c0e6f0ee
shraddha-ghadage <shrghadage@gmail.com>
2024-12-12 20:40:53 +0530

    sdk added

---
+++ SmartechNCE/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  SmartechNCE
+//
+//  Created by JustMac on 12/12/24.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
